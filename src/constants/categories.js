f10ddc0eae8f778186445ad3bdcd6b28da847914
Andres Obregon <andresnicolasobregon@gmail.com>
2022-03-27 10:26:33 -0300

    configuracion de header

---
+++ src/constants/categories.js
@@ -0,0 +1,26 @@
+export const CATEGORIES = [
+    {
+        id: 1,
+        name: 'Harinas Puras',
+        color: '#84dcc6',
+        description: 'Panes de harinas puras'
+    },
+    {
+        id: 2,
+        name: 'Harinas Integrales',
+        color: '#d6edff',
+        description: 'Panes de harinas integrales'
+    },
+    {
+        id: 3,
+        name: 'Harinas de Trigo',
+        color: '#acd7ec',
+        description: 'Panes de harinas de trigo'
+    },
+    {
+        id: 4,
+        name: 'Harinas de Maíz',
+        color: '#8b95c9',
+        description: 'Panes de harinas de maíz'
+    },
+]
--- src/navigation/main.js
+++ src/navigation/main.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { Platform } from 'react-native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from '../screens/home/index'
 import Category from '../screens/category/index'
 import Product from '../screens/product/index'
+import { colors } from '../constants/themes';
 
 const Stack = createNativeStackNavigator();
 
 const MainNavigator = () => {
     return (
-        <Stack.Navigator initialRouteName='Home'>
+        <Stack.Navigator
+            initialRouteName='Home'
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: Platform.OS === 'android' ? colors.primaryColor : '',
+                },
+                headerTintColor: Platform.OS === 'android' ? 'white' : colors.primaryColor,
+                headerTitleStyle: {
+                    fontFamily: 'OpenSans-Bold',
+                    fontSize: 17
+                }
+            }}
+        >
             <Stack.Screen
                 name='Home'
                 component={Home} 
@@ -17,10 +31,12 @@ const MainNavigator = () => {
             <Stack.Screen 
                 name='Category'
                 component={Category}
+                options={({route}) => ({ title: route.params.name })}
             />
             <Stack.Screen 
                 name='Product'
                 component={Product} 
+                options={({route}) => ({ title: route.params.name })}
              />
         </Stack.Navigator>
         )
